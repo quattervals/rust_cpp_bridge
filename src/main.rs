a461ede7683ffe7082131ade9d67908308c5fdd7
Oliver With <oliverwith@bbv.ch>
2025-06-11 10:16:27 +0200

    Wrapper for FFI code so that the person feels idiomatic in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use cxx::let_cxx_string;
+use cxx::{let_cxx_string, UniquePtr};
 
 #[cxx::bridge(namespace = "prs")]
 mod ffi {
-
     // C++ types and signatures exposed to Rust.
     unsafe extern "C++" {
         include!("person.hpp");
@@ -11,22 +10,47 @@ mod ffi {
 
         fn newPerson(name: &CxxString, zip: &CxxString, dob: u32) -> UniquePtr<Person>;
 
-
         fn getAge(self: &Person) -> u32;
         fn getZip(self: &Person) -> &CxxString;
         fn updateZip(self: Pin<&mut Person>, zip: &CxxString);
     }
 }
 
+/// A Rust wrapper for the C++ Person class that provides a clean, idiomatic API
+pub struct Person {
+    inner: UniquePtr<ffi::Person>,
+}
+
+impl Person {
+    pub fn new(name: &str, zip: &str, dob: u32) -> Self {
+        let_cxx_string!(cxx_name = name);
+        let_cxx_string!(cxx_zip = zip);
+
+        Self {
+            inner: ffi::newPerson(&cxx_name, &cxx_zip, dob),
+        }
+    }
+
+    pub fn age(&self) -> u32 {
+        self.inner.getAge()
+    }
+
+    pub fn zip(&self) -> String {
+        self.inner.getZip().to_string()
+    }
+
+    pub fn update_zip(&mut self, zip: &str) {
+        let_cxx_string!(cxx_zip = zip);
+        self.inner.as_mut().unwrap().updateZip(&cxx_zip);
+    }
+}
+
 fn main() {
-    let_cxx_string!(some_person = "Susi Bluemli");
-    let_cxx_string!(some_zip = "698");
-    let mut person = ffi::newPerson(&some_person, &some_zip, 1965);
-    println!("Current age of person is {}", person.getAge());
+    let mut person = Person::new("Susi Bluemli", "698", 1965);
+    println!("Current age of person is {}", person.age());
 
+    person.update_zip("667");
+    println!("Zip of person is {}", person.zip());
 
-    let_cxx_string!(updated_zip = "667");
-    person.as_mut().unwrap().updateZip(&updated_zip);
-
-    println!("Zip of person is {}", person.getZip());
+    println!("Person details: age={}, zip={}", person.age(), person.zip());
 }
